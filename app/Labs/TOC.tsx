8ae347d542d8cfe788df82184eb4a8b5b258783f
vardhanreddy53 <vardhanreddy53@gmail.com>
2025-09-17 22:47:07 -0400

    first assignment

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,26 +1,16 @@
 import Link from "next/link";
 export default function TOC() {
  return (
-   <ul>
-     <li>
+  <div>
        <Link href="/Labs" id="wd-Home-link">
-         Home </Link>
-     </li>
-     <li>
+         Home </Link><br/>
        <Link href="/Labs/lab1" id="wd-lab1-link">
-         Lab 1 </Link>
-     </li>
-     <li>
+         Lab 1 </Link><br/>
        <Link href="/Labs/lab2" id="wd-lab2-link">
-         Lab 2 </Link>
-     </li>
-     <li>
+         Lab 2 </Link><br/>
        <Link href="/Labs/lab3" id="wd-lab3-link">
-        Lab 3</Link>
-     </li>
-      <li>
+        Lab 3</Link><br/>
        <Link href="/" id="wd-Kambaz-link">
          Kambaz</Link>
-     </li>
-   </ul>
+         </div>
 );}
