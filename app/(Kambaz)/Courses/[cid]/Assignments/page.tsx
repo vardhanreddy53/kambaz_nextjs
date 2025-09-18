e6b925896c5b599fb97ec56e3bc8a8bbb72ad93f
vardhanreddy53 <vardhanreddy53@gmail.com>
2025-09-17 20:50:01 -0400

    first assignment

--- app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
+++ app/(Kambaz)/Courses/[cid]/Assignments/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import Link from 'next/link';
 
 export default function Assignments() {
   return (
@@ -14,84 +14,84 @@ export default function Assignments() {
       </h3>
       <ul id="wd-assignment-list">
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/123"
           >
             A1 - ENV + HTML
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until May 6 at 12:00am | Due May
             13 at 11:59pm | 100 pts
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/124"
           >
             A2 - CSS + BOOTSTRAP
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until May 13 at 12:00am | Due May
             20 at 11:59pm | 100 pts
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/125"
           >
             A3 - JAVASCRIPT + REACT
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until May 20 at 12:00am | Due May
             27 at 11:59pm | 100 pts
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/126"
           >
             A4 - SERVER-SIDE DEVELOPMENT
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until May 27 at 12:00am | Due Jun
             3 at 11:59pm | 100 pts
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/127"
           >
             A5 - DATABASE INTEGRATION
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until Jun 3 at 12:00am | Due Jun
             10 at 11:59pm | 100 pts
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/128"
           >
             A6 - AUTHENTICATION & SECURITY
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until Jun 10 at 12:00am | Due Jun
             17 at 11:59pm | 100 pts
           </div>
         </li>
         <li className="wd-assignment-list-item">
-          <a
+          <Link
             className="wd-assignment-link"
             href="/Courses/1234/Assignments/129"
           >
             A7 - DEPLOYMENT
-          </a>
+          </Link>
           <div>
             Multiple Modules | Not available until Jun 17 at 12:00am | Due Jun
             24 at 11:59pm | 100 pts
