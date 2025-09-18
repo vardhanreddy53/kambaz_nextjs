3429d6b9fc89933f7423583f667bd4184b7ab00c
vardhanreddy53 <vardhanreddy53@gmail.com>
2025-09-17 22:27:41 -0400

    first assignment

--- app/(Kambaz)/page.tsx
+++ app/(Kambaz)/page.tsx
@@ -2,6 +2,7 @@ export default function Kambaz(){
   return (
     <div id="wd-kambaz">
       <h1>Kambaz</h1>
+      <a href="https://github.com/vardhanreddy53/kambaz_nextjs">Github</a>
       <p>Hi My Name is Sai Vardhan Reddy Pathuri Currently pursuing my master in computer science from Northeastern university i have joined NEU for Fall 2025 and taken CS5610202610 WebDev By Jose Annunziato</p>
     </div>
 );}
