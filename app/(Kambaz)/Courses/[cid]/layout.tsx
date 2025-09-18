a0b2bc57dfd734527daf5f68286a1343a7838354
vardhanreddy53 <vardhanreddy53@gmail.com>
2025-09-17 21:11:29 -0400

    first assignment 1

--- app/(Kambaz)/Courses/[cid]/layout.tsx
+++ app/(Kambaz)/Courses/[cid]/layout.tsx
@@ -1,19 +1,27 @@
 import { ReactNode } from "react";
 import CourseNavigation from "./Navigation";
-export default async function CoursesLayout(
-  { children, params }: Readonly<{ children: ReactNode; params: Promise<{ id: string }> }>) {
- const {cid} = await params;
- return (
-   <div id="wd-courses">
-     <h2>Courses {cid}</h2>
-     <hr />
-     <table>
-       <tbody>
-         <tr>
-           <td valign="top" width="200"> <CourseNavigation /> </td>
-           <td valign="top" width="100%"> {children} </td>
-         </tr>
-       </tbody>
-     </table>
-   </div>
-);}
+
+export default function CoursesLayout(
+  { children, params }: { children: ReactNode; params: { cid: string } }
+) {
+  const { cid } = params;
+  return (
+    <div id="wd-courses">
+      <h2>Courses {cid}</h2>
+      <hr />
+      <table>
+        <tbody>
+          <tr>
+            <td valign="top" style={{ width: 200 }}>
+              <CourseNavigation />
+            </td>
+            <td valign="top" style={{ width: "100%" }}>
+              {" "}
+              {children}{" "}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
