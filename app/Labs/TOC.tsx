a190b659911d9bf777329771a26f7fbc8c57ed46
vardhanreddy53 <vardhanreddy53@gmail.com>
2025-10-09 18:06:55 -0400

    second assignment

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -1,41 +1,26 @@
-"use client";
-import { ListGroup } from "react-bootstrap";
+import { Nav, NavItem, NavLink } from "react-bootstrap";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
-
 export default function TOC() {
-  const pathname = usePathname();
+ return (
+   <Nav variant="pills">
+     <NavItem>
+       <NavLink href="/Labs" as={Link}>Labs</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Labs/Lab1" as={Link}>Lab 1</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Labs/Lab2" as={Link}>Lab 2</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/Labs/Lab3" as={Link}>Lab 3</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="/" as={Link}>Kambaz</NavLink>
+     </NavItem>
+     <NavItem>
+       <NavLink href="https://github.com/">My GitHub</NavLink>
+     </NavItem>
+   </Nav>
+);}
 
-  return (
-    <ListGroup>
-      <Link href="/Labs" style={{ textDecoration: 'none' }}>
-        <ListGroup.Item action active={pathname === "/Labs"}>
-          Labs
-        </ListGroup.Item>
-      </Link>
-      <Link href="/Labs/lab1" style={{ textDecoration: 'none' }}>
-        <ListGroup.Item action active={pathname === "/Labs/lab1"}>
-          Lab 1
-        </ListGroup.Item>
-      </Link>
-      <Link href="/Labs/lab2" style={{ textDecoration: 'none' }}>
-        <ListGroup.Item action active={pathname === "/Labs/lab2"}>
-          Lab 2
-        </ListGroup.Item>
-      </Link>
-      <Link href="/Labs/lab3" style={{ textDecoration: 'none' }}>
-        <ListGroup.Item action active={pathname === "/Labs/lab3"}>
-          Lab 3
-        </ListGroup.Item>
-      </Link>
-      <Link href="/" style={{ textDecoration: 'none' }}>
-        <ListGroup.Item action>
-          Kambaz(kanbas typo ig)
-        </ListGroup.Item>
-      </Link>
-      <ListGroup.Item action href="https://github.com/vardhanreddy53" target="_blank">
-        My GitHub
-      </ListGroup.Item>
-    </ListGroup>
-  );
-}
